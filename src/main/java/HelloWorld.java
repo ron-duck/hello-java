b7ea21a952fbbae46a94424fbb55abbc8954c252
ron-duck <ronald.felice@blackduck.com>
2025-03-07 10:27:41 -0500

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,12 +1,21 @@
+// public class HelloWorld {
+//    public static void main(String[] args) {
+//        String secret = null;
+//            try { 
+//                javax.crypto.spec.SecretKeySpec keyspec = new javax.crypto.spec.SecretKeySpec(secret.getBytes("UTF-8"), "AES"); 
+//            }
+//        catch (Exception e) { 
+//            System.out.println("Something went wrong."); 
+//        }
+//        System.out.println(secret);
+//    }
+//}
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class HelloWorld {
+    private static final Logger logger = LogManager.getLogger("HelloWorld");
     public static void main(String[] args) {
-        String secret = null;
-            try { 
-                javax.crypto.spec.SecretKeySpec keyspec = new javax.crypto.spec.SecretKeySpec(secret.getBytes("UTF-8"), "AES"); 
-            }
-        catch (Exception e) { 
-            System.out.println("Something went wrong."); 
-        }
-        System.out.println(secret);
+        logger.info("Hello, World!");
     }
 }
