8eb1e6b533b21b19d21520cd92ba703239c0d734
ron-duck <ronald.felice@blackduck.com>
2025-03-07 10:08:27 -0500

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        String secret = null;
+        String secret = "Not a secret";
             try { 
                 javax.crypto.spec.SecretKeySpec keyspec = new javax.crypto.spec.SecretKeySpec(secret.getBytes("UTF-8"), "AES"); 
             }
